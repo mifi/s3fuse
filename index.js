4e279dced47e55223627e879b2650bb9c19925e1
Mikael Finstad <finstaden@gmail.com>
2018-10-16 21:36:29 +0200

    impl read

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const cli = meow(`
   Usage
-    $ s3fuse bucket /mnt/path
+    $ s3fuse bucket-name /mnt/path
 
   Options
 `);
@@ -26,6 +26,19 @@ const mountPath = cli.input[1];
 // TODO cleanup after some time to prevent overgrowing
 // TODO invalidate cache logic
 const globalCache = {};
+
+const fds = {};
+let fdCounter = 0;
+
+function openFile(path) {
+  fdCounter++;
+  fds[fdCounter] = path;
+  return fdCounter;
+}
+
+function getFdPath(fd) {
+  return fds[fd];
+}
 
 function getCached(path) {
   return globalCache[path];
@@ -92,20 +105,22 @@ fuse.mount(mountPath, {
 
       const { files, subdirs } = cached;
       const allEntries = [...files.map(f => f.name), ...subdirs];
+      console.log('readdir result', allEntries);
       return cb(0, allEntries);
     } catch (err) {
       console.error(err);
       cb(fuse.EIO);
     }
   },
+
   getattr: async (path, cb) => {
     try {
       console.log('getattr(%s)', path)
 
-      if (path === '/') {
+      /* if (path === '/') {
         cb(0, getDirAttrs())
         return
-      }
+      } */
 
       let cached = getCached(path);
 
@@ -113,16 +128,32 @@ fuse.mount(mountPath, {
       const match = path.match('(.+)/([^/]+)$');
       if (match) {
         const parentDir = match[1];
-        const parentCached = getCached(parentDir);
-        if (parentCached) {
-          const fileName = match[2];
-          console.log({ parentDir, fileName });
-          console.log({ parentCached });
-          if (![...parentCached.files.map(f => f.name), ...parentCached.subdirs].includes(fileName)) {
-            console.log('Not found in cached parent', path);
-            return cb(fuse.ENOENT);
-          }
+        let parentCached = getCached(parentDir);
+
+        if (!parentCached) {
+          console.log('getattr parent not cached, need to fetch', path);
+          parentCached = await fetchPath(path);
         }
+
+        const fileName = match[2];
+        // console.log({ parentDir, fileName });
+        // console.log('parentCached files:', parentCached.files, 'dirs:', parentCached.dirs);
+
+        const dir = parentCached.subdirs.find(d => d === fileName);
+        const file = parentCached.files.find(f => f.name === fileName);
+
+        if (dir) {
+          console.log('path is a directory', path);
+          return cb(0, getDirAttrs());
+        }
+
+        if (file) {
+          console.log('path is a file', path);
+          return cb(0, getFileAttrs(file.size));
+        }
+
+        console.log('Not found in cached parent', path);
+        return cb(fuse.ENOENT);
       }
 
       if (!cached) {
@@ -130,6 +161,7 @@ fuse.mount(mountPath, {
         cached = await fetchPath(path);
       }
 
+      // TODO we can remove this, no?
       if (cached.files.length > 1 || cached.subdirs.length > 0) {
         console.log('path is a directory', path);
         return cb(0, getDirAttrs());
@@ -146,15 +178,39 @@ fuse.mount(mountPath, {
   },
 
   open: function (path, flags, cb) {
-    console.log('open(%s, %d)', path, flags)
-    cb(0, 42) // 42 is an fd
+    try {
+      if (flags & 3 !== 0) return cb(fuse.EIO);
+      // TODO flags: only read supported, else return err
+      console.log('open(%s, %d)', path, flags);
+      const fd = openFile(path);
+      cb(0, fd);
+    } catch (err) {
+      console.error(err);
+      cb(fuse.EIO);
+    }
   },
-  read: function (path, fd, buf, len, pos, cb) {
-    console.log('read(%s, %d, %d, %d)', path, fd, len, pos)
-    var str = 'hello world\n'.slice(pos, pos + len)
-    if (!str) return cb(0)
-    buf.write(str)
-    return cb(str.length)
+
+  read: async (path, fd, buf, len, pos, cb) => {
+    try {
+      console.log('read(%s, %d, %d, %d)', path, fd, len, pos)
+
+      // const path = getFdPath(fd);
+      // if (!path) return cb(fuse.EIO);
+
+      const rangeFrom = pos;
+      const rangeTo = pos + len;
+
+      const resp = await s3.getObject({ Bucket: bucket, Key: path.replace(/^\//, ''), Range: `bytes=${rangeFrom}-${rangeTo}` }).promise();
+      console.log('s3 get response:', resp);
+      const respData = resp.Body;
+
+      const respLen = Math.min(len, respData.length);
+      if (respData.length > 0) respData.copy(buf, 0, 0, respLen);
+      return cb(respLen);
+    } catch (err) {
+      console.error(err);
+      cb(fuse.EIO);
+    }
   }
 }, function (err) {
   if (err) throw err
